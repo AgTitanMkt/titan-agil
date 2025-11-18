4b3882046ba5b5616706ce5d7bca3543deb4151b
andre.av <andre.veras.3@globalhitss.com.br>
2025-11-18 00:06:46 -0300

    feat: cron de atualização redtrack

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('sync:redtrack --from="'.Carbon::now()->format('Y-m-d').'" --to="'.Carbon::now()->format('Y-m-d').'"')
+    ->everyThreeHours()
+    ->withoutOverlapping()
+    ->runInBackground()
+    ->sendOutputTo(storage_path('logs/sync_redtrack.log'));
+
