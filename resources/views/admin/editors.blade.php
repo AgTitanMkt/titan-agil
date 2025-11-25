cd40b52f4b2a898bd9a458024d93ea6aa2965c0f
andre.av <andre.veras.3@globalhitss.com.br>
2025-11-25 00:10:37 -0300

    fix: vite

--- resources/views/admin/editors.blade.php
+++ resources/views/admin/editors.blade.php
@@ -2,7 +2,6 @@
 
 
     {{-- ARQUIVO --}}
-    <link rel="stylesheet" href="{{ asset('css/admin-editors.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 
     <div class="header-container">
